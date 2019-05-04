2625a023c8c1ab1d3a1fb12e953b474fe92c85de
zoy2control <paulino_zoy_it@163.com>
2019-05-04 16:33:43 +0800

    markdown 语法 v0.1

---
+++ src/main/java/com/zoy/summary/use/ZoySummaryUseApplication.java
@@ -0,0 +1,13 @@
+package com.zoy.summary.use;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoySummaryUseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoySummaryUseApplication.class, args);
+	}
+
+}
